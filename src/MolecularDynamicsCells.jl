c1f38622726aa5d5f3908b9976b9a983afbc1801
Edwin Bedolla <developeredwin@gmail.com>
2025-03-04 15:59:55 +0100

    Started a refactor on the base code

--- src/MolecularDynamicsCells.jl
+++ src/MolecularDynamicsCells.jl
@@ -159,22 +159,3 @@ function simulation(params::Parameters, pathname; eq_steps=100_000, prod_steps=5
 
     return nothing
 end
-
-function main()
-    # densities = [0.776, 0.78, 0.82, 0.84, 0.86, 0.9]
-    # reps = 10
-    densities = [0.9]
-    ktemp = 0.85
-
-    for d in densities
-        params = Parameters(d, ktemp, 4000)
-        # Create a new directory with these parameters
-        pathname = joinpath(@__DIR__, "density=$(@sprintf("%.4g", d))")
-        mkpath(pathname)
-        simulation(params, pathname; eq_steps=100_000, prod_steps=1_000_000)
-    end
-
-    return nothing
-end
-
-main()
